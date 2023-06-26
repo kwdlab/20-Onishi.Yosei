f7e4d9d6d6095cbe08ed8a901d9031a1771ff54c
k433386 <k433386@kansai-u.ac.jp>
2023-06-26 09:36:55 +0900

    test
    test

--- paiza/定番アルゴリズムの習得/02-素朴なソートアルゴリズムメニュー/Main.scala
+++ paiza/定番アルゴリズムの習得/02-素朴なソートアルゴリズムメニュー/Main.scala
@@ -2,6 +2,22 @@ import scala.io.StdIn._
 
 object Main extends App {
 
-}
+    val n = readLine().toInt
+    val An = readLine().split(" ").map(_.toInt)
 
-// end of file
+    def insertionSort(A: Array[Int], n: Int) = {
+        for (i <- 1 until n){
+            val x = A(i)
+            var j = i - 1
+
+            while (j >= 0 && A(j) > x){
+                A(j+1) = A(j)
+                j = j - 1
+            }
+            A(j+1) = x
+            println(A.mkString(" "))
+        }
+    }
+
+    insertionSort(An, n)
+}
