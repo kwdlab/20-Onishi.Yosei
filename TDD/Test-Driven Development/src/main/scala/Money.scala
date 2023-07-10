9918d80d7557921cb97d13719cbab591548e6713
k433386 <k433386@kansai-u.ac.jp>
2023-07-10 16:47:13 +0900

    0710
    test

--- TDD/Test-Driven Development/src/main/scala/Money.scala
+++ TDD/Test-Driven Development/src/main/scala/Money.scala
@@ -5,4 +5,8 @@ class Money(protected val amount: Int) {
     def equals(money: Money): Boolean = {
         amount == money.amount && getClass().equals(money.getClass())
     }
+
+    def dollar: Dollar = {
+        new Dollar(amount)
+    }
 }
