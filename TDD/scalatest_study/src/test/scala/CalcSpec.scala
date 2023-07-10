bfa16309c213ef3953b39e9055e063cfc83791eb
k433386 <k433386@kansai-u.ac.jp>
2023-07-10 14:08:02 +0900

    0710
    test

---
+++ TDD/scalatest_study/src/test/scala/CalcSpec.scala
@@ -0,0 +1,18 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.diagrams.Diagrams
+
+class CalcSpec extends AnyFlatSpec with Diagrams {
+
+  val calc = new Calc
+
+  "sum関数" should "整数の配列を取得し、それらを足し合わせた整数を返すことができる" in {
+    assert(calc.sum(Seq(1, 2, 3)) === 6)
+    assert(calc.sum(Seq(0)) === 0)
+    assert(calc.sum(Seq(-1, 1)) === 0)
+    assert(calc.sum(Seq()) === 0)
+  }
+
+  it should "Intの最大を上回った際にはオーバーフローする" in {
+    assert(calc.sum(Seq(Integer.MAX_VALUE, 1)) === Integer.MIN_VALUE)
+  }
+}
